6d19d05062dc04b0a82c135bfa158a30e7176ca9
daiivasq <daihianav@gmail.com>
2021-10-07 09:45:32 -0400

    Using Package and Console Structure.

---
+++ MorseTraductor/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MorseTraductor
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("######--TEXTO A MORSE--#####");
+            Console.WriteLine("Este simbolo -> '|' indica la separacion de caracteres.\n Este simbolo -> '#' indica espacio entre caracteres.");
+            MorseTranslate();
+        }
+
+        public static void MorseTranslate()
+        {
+            var text = "";
+            while (text != "@")
+            {
+                Console.WriteLine("Pulse [@] si desea salir.\nDigite una cadena de texto:");
+                text = Console.ReadLine();
+
+                var parameter = MorseLibrary.Morse.Converter(text);
+                foreach (var item in parameter)
+                {
+                    Console.Write($"{item}");
+                }
+
+            }
+        }
+    }
+}
